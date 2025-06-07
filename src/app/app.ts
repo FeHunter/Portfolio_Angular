f915b4bfaad35cbb1ebeea4591d1fb8c83922036
FeHunter <hunterfe1111@gmail.com>
2025-06-07 14:39:03 -0300

    Criando home-page layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// pages
+import { HomePage } from './pages/home-page/home-page';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
